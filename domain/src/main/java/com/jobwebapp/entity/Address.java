e3a378c60ea3292ea28082ab58bee24582a7579a
Lukasz <kuchtalukasz89@gmail.com>
2020-04-22 00:05:00 +0200

    commit before merge

--- domain/src/main/java/com/jobwebapp/entity/Address.java
+++ domain/src/main/java/com/jobwebapp/entity/Address.java
@@ -3,7 +3,7 @@ package com.jobwebapp.entity;
 
 import lombok.*;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Data
 @NoArgsConstructor
@@ -11,13 +11,24 @@ import javax.persistence.Entity;
 @ToString
 @Builder
 @Entity
+@Table(name = "address")
 public class Address {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "country")
     private String country;
+    @Column(name = "province")
     private String province;
+    @Column(name = "city")
     private String city;
+    @Column(name = "street")
     private String street;
+    @Column(name = "building_number")
     private int buildingNumber;
+    @Column(name = "post_code")
     private String postCode;
 
 }
